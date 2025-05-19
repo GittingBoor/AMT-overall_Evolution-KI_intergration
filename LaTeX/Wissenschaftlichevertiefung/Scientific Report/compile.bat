d6febed493715582b8c147fca9857764dcd60321
GittingBoor <benedikt.kolodziej@web.de>
2025-05-19 16:59:45 +0200

    Recherche

--- LaTeX/Wissenschaftlichevertiefung/Scientific Report/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir auxil 2>nul
-mkdir out 2>nul
-
-pdflatex -interaction=nonstopmode -output-directory=out -aux-directory=auxil main.tex
-biber --input-directory=auxil --output-directory=out main
-pdflatex -interaction=nonstopmode -output-directory=out -aux-directory=auxil main.tex
-pdflatex -interaction=nonstopmode -output-directory=out -aux-directory=auxil main.tex
-python wordcount.py
